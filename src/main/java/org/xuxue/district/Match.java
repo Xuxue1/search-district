6df60827632d2785afd9217572fafe2d42f6f850
许继强 <xujiqiang@aidigger.com>
2020-06-11 20:20:26 +0800

    init

---
+++ src/main/java/org/xuxue/district/Match.java
@@ -0,0 +1,10 @@
+package org.xuxue.district;
+
+public class Match {
+
+
+    public static String match(String district, String detail) {
+        return null;
+    }
+
+}
---
+++ src/main/java/org/xuxue/district/Pattern.java
@@ -0,0 +1,26 @@
+package org.xuxue.district;
+
+import java.util.HashMap;
+
+public class Pattern {
+
+    private HashMap<String, >
+
+
+    public Pattern() {
+
+    }
+
+
+
+    private void compile() {
+
+    }
+
+    private static class Tree {
+
+
+
+    }
+
+}
